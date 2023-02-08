22633b7f8b07e93d499e1463cdaa93feddf3acad
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2023-02-07 18:26:46 -0600

    Use webdriver

--- CustomSelenium.py
+++ CustomSelenium.py
@@ -1,14 +1,31 @@
 from typing import List
 
-from RPA.Browser.Selenium import Selenium
-from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import By, WebElement
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-class CustomSelenium(Selenium):
+class CustomSelenium(Chrome):
 
-    def search_for(self, locator: str, term: str) -> bool:
-        self.input_text(locator, term)
-        self.press_keys(locator, "ENTER")
+    def __init__(self):
+        super().__init__(service=ChromeService(ChromeDriverManager().install()))
+
+    def input_text(self, locator: str, term: str) -> WebElement:
+        element = self.find_element(by=By.XPATH, value=locator)
+        element.send_keys(term)
+        return element
+
+    def search_for(self, locator: str, term: str) -> WebElement:
+        element = self.input_text(locator, term)
+        element.send_keys(Keys.ENTER)
+        return element
+
+    def click_element(self, locator: str) -> WebElement:
+        element = self.find_element(by=By.XPATH, value=locator)
+        element.click()
+        return element
 
     def get_element_in_element(self, by: str, locator: str, element: WebElement) -> WebElement or None:
         try:
@@ -18,12 +35,12 @@ class CustomSelenium(Selenium):
 
     def get_element(self, locator: str) -> WebElement or None:
         try:
-            return self.get_webelement(locator)
+            return self.find_element(by=By.XPATH, value=locator)
         except:
             return None
 
-    def get_elements(self, locator: str) -> List[WebElement] or None:
+    def get_elements(self, locator: str) -> List[WebElement]:
         try:
-            return self.get_webelements(locator)
+            return self.find_elements(by=By.XPATH, value=locator)
         except:
-            return None
+            return []
--- NYTBot.py
+++ NYTBot.py
@@ -59,10 +59,10 @@ class NYTBot:
     # Search Term
     def search_term(self):
         try:
-            self.seh.click_button(
-                "xpath: //*[@data-test-id='search-button']")
+            self.seh.click_element(
+                "//*[@data-test-id='search-button']")
             self.seh.search_for(
-                "xpath: //*[@data-testid='search-input']", self.term)
+                "//*[@data-testid='search-input']", self.term)
         except:
             raise BotException('Error searching for a term')
 
@@ -70,15 +70,15 @@ class NYTBot:
     def set_date_range(self):
         try:
             startDate, endDate = self.get_dates(self.months_number)
-            self.seh.click_button(
-                "xpath: //*[@data-testid='search-date-dropdown-a']")
-            self.seh.click_button("xpath: //*[@value='Specific Dates']")
+            self.seh.click_element(
+                "//*[@data-testid='search-date-dropdown-a']")
+            self.seh.click_element("//*[@value='Specific Dates']")
             self.seh.input_text(
-                "xpath: //*[@data-testid='DateRange-startDate']", startDate)
+                "//*[@data-testid='DateRange-startDate']", startDate)
             self.seh.input_text(
-                "xpath: //*[@data-testid='DateRange-endDate']", endDate)
-            self.seh.click_button(
-                "xpath: //*[@data-testid='search-date-dropdown-a']")
+                "//*[@data-testid='DateRange-endDate']", endDate)
+            self.seh.click_element(
+                "//*[@data-testid='search-date-dropdown-a']")
             time.sleep(1)
         except:
             raise BotException('Error setting date range')
@@ -87,10 +87,10 @@ class NYTBot:
     def set_section(self):
         if self.section != 'Any':
             try:
-                self.seh.click_button(
-                    "xpath: //div[@data-testid='section']//button[@data-testid='search-multiselect-button']")
                 self.seh.click_element(
-                    "xpath: //*[contains(@value, '{}')]".format(self.section))
+                    "//div[@data-testid='section']//button[@data-testid='search-multiselect-button']")
+                self.seh.click_element(
+                    "//*[contains(@value, '{}')]".format(self.section))
                 time.sleep(1)
             except:
                 raise BotException('Error setting section')
@@ -99,17 +99,17 @@ class NYTBot:
     def sort_by_newest(self):
         try:
             self.seh.click_element(
-                "xpath: //*[contains(text(), 'Sort by Newest')]")
+                "//*[contains(text(), 'Sort by Newest')]")
             time.sleep(1)
         except:
             raise BotException('Error sorting by newest')
 
     # Show all results
     def show_all_results(self):
-        locator = "xpath: //button[@data-testid='search-show-more-button']"
+        locator = "//button[@data-testid='search-show-more-button']"
         try:
             while True:
-                self.seh.click_button(locator)
+                self.seh.click_element(locator)
                 time.sleep(1)
         except Exception as e:
             print(e)
@@ -118,7 +118,7 @@ class NYTBot:
     def get_all_information(self):
         self.results = []
         results = self.seh.get_elements(
-            "xpath: //ol[@data-testid='search-results']//li[@data-testid='search-bodega-result']")
+            "//ol[@data-testid='search-results']//li[@data-testid='search-bodega-result']")
         for element in results:
             row = {}
             data = element.text.split('\n')
@@ -174,8 +174,7 @@ class NYTBot:
     # Run Bot
     def run(self):
         try:
-            self.seh.open_available_browser(
-                self.url, browser_selection='Chrome')
+            self.seh.get(self.url)
             self.search_term()
             self.set_date_range()
             self.set_section()
@@ -184,4 +183,4 @@ class NYTBot:
             self.get_all_information()
             self.save_data()
         finally:
-            self.seh.close_all_browsers()
+            self.seh.quit()
