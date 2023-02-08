701bf2ba28ed5693bdb2316532ae1e3d9e8b09aa
Miguel219 <34550700+Miguel219@users.noreply.github.com>
2023-02-07 20:43:55 -0600

    Update NYTBot.py

--- NYTBot.py
+++ NYTBot.py
@@ -109,7 +109,6 @@ class NYTBot:
         locator = "xpath: //button[@data-testid='search-show-more-button']"
         try:
             while True:
-                self.seh.wait_until_element_is_visible(locator)
                 self.seh.click_button(locator)
                 time.sleep(1)
         except Exception as e:
